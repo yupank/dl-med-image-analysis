ab3c534592eb7d5bad0ad209104eead94cc2e7d8
Yuriy Pankratov <32850110+yupank@users.noreply.github.com>
2023-10-19 12:25:59 +0100

    added utility functions

---
+++ cnn_models.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+
+
+class Cnn3Layer(nn.Module):
+    """ The scalable CNN of 3 convolutional 2D units followed by a single linear layer with sigmoid output function.
+        Model takes the data as stacks of 2D-images with n channels  (5 for the current dataset), i.e. as 3D tensors.
+        As activation functions, both ReLU and LeakyReLU work fine, with sligtly smoother conversion with the LeakyReLU
+        Parameters:
+            init_nodes |int - number of nodes in the 1st convolutional layer;  the number of nodes 
+                in next conv units expands two-fold and in the unit 3 can be increased or decreased;
+            conv_3_scale | int - scaling factor for number of nodes in the conv layer 3;
+            conv_kernel | int - size of kernel (filter) for all Conv layers
+            n_channels |int - number of channels in the input image;
+            inp_dim | int - number of pixels in the input image;
+            drop | float - the probablity for all Dropout layer
+
+    """
+    def __init__(self, init_nodes=16, conv_3_scale = 4, n_channel=5, conv_kernel= 5, inp_dim=28*28, drop=0.1):
+        super(Cnn3Layer, self).__init__()
+
+        self.name = f'2D_CNN_3L_{init_nodes}_nodes'
+        pad = int((conv_kernel-1)/2)
+        self.conv_1 = nn.Sequential(
+            nn.Conv2d(n_channel, init_nodes, kernel_size= conv_kernel, stride=1, padding=pad),
+            nn.BatchNorm2d(init_nodes),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(drop)
+        )
+        self.conv_2 = nn.Sequential(
+            nn.Conv2d(init_nodes, init_nodes*2, kernel_size= conv_kernel, stride=1, padding=pad),
+            nn.BatchNorm2d(init_nodes*2),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(drop)
+        )
+        
+        self.conv_3 = nn.Sequential(
+            nn.Conv2d(init_nodes*2, init_nodes*conv_3_scale, kernel_size= conv_kernel, stride=1, padding=pad),
+            nn.BatchNorm2d(init_nodes*conv_3_scale),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(drop)
+
+        )
+        out_dim = int(inp_dim/64)
+        self.lin_1 = nn.Linear(init_nodes*conv_3_scale*out_dim,1)
+        self.final = nn.Sigmoid()
+
+    def forward(self,x):
+        out = self.conv_1(x)
+        out = self.conv_2(out)
+        out = self.conv_3(out)
+        out = out.view(out.size(0), -1)
+        out = self.lin_1(out)
+        out = self.final(out)
+        return out.squeeze(1)
+    
+
+class Cnn4LayerSiam(nn.Module):
+    """ CNN similar to the above but with 4 convolutional units
+        Model has a "siamese" structure with nodes numbers increasing from unit 1 to 2 and then decreasing towards unit 4
+    """
+    def __init__(self, init_nodes=8, n_channel=3, conv_kernel= 5, inp_dim=28*28, drop= 0.1):
+        super(Cnn4LayerSiam, self).__init__()
+        self.name = f'2D_CNN_4L{init_nodes}_nodes'
+        pad = int((conv_kernel-1)/2)
+        self.conv_1 = nn.Sequential(
+            nn.Conv2d(n_channel, init_nodes, kernel_size= conv_kernel, stride=1, padding=pad),
+            nn.BatchNorm2d(init_nodes),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(drop)
+        )
+        self.conv_2 = nn.Sequential(
+            nn.Conv2d(init_nodes, init_nodes*2, kernel_size= conv_kernel, stride=1, padding=pad),
+            nn.BatchNorm2d(init_nodes*2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(drop)
+        )
+        self.conv_3 = nn.Sequential(
+            nn.Conv2d(init_nodes*2, init_nodes*2, kernel_size= conv_kernel, stride=1, padding=pad),
+            nn.BatchNorm2d(init_nodes*2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(drop)
+        )
+        self.conv_4 = nn.Sequential(
+            nn.Conv2d(init_nodes*2, init_nodes, kernel_size= conv_kernel, stride=1, padding=pad),
+            nn.BatchNorm2d(init_nodes),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Flatten()
+        )
+        out_dim = int(inp_dim/256)
+        self.lin_1 = nn.Linear(init_nodes*out_dim,1)
+        self.final = nn.Sigmoid()
+
+    def forward(self,x):
+        out = self.conv_1(x)
+        out = self.conv_2(out)
+        out = self.conv_3(out)
+        out = self.conv_4(out)
+        out = self.lin_1(out)
+        out = self.final(out)
+        return out.squeeze(1)
+    
+
+class Cnn3Layer3d(nn.Module):
+    """ The scalable CNN of 2 convolutional 3D units followed by two fully connected linear units and sigmoid output function.
+        Model takes the data as 3D images as n channels (1 for the current dataset), i.e. as 4D tensors.
+        As activation functions, both ReLU and LeakyReLU work fine, with sligtly smoother conversion with the LeakyReLU
+        Parameters:
+            init_nodes |int - number of nodes in the 1st convolutional layer;  
+                the number of nodes in next conv units expands two-fold;
+            conv_kernel | int - size of kernel (filter) for Conv3d layers
+            n_channels |int - number of channels in the input image;
+            inp_dim | int - number of pixels in the input image;
+            drop | float - the probablity for all Dropout layer
+    """  
+    def __init__(self, init_nodes=16, n_channel=1, conv_kernel= 3, inp_dim=32*32, drop=0.1):
+        super(Cnn3Layer3d, self).__init__()
+
+        self.name = f'3D_CNN_{init_nodes}_nodes'
+        pad = int((conv_kernel-1)/2)
+        self.conv_unit_1 = nn.Sequential(
+            nn.Conv3d(n_channel, init_nodes, kernel_size= conv_kernel, stride=1, padding=pad),
+            nn.BatchNorm3d(init_nodes),
+            nn.LeakyReLU(),
+            nn.MaxPool3d(kernel_size=2, stride=2),
+            nn.Dropout(drop)
+        )
+        self.conv_unit_2 = nn.Sequential(
+            nn.Conv3d(init_nodes, init_nodes*2, kernel_size= conv_kernel, stride=1, padding=pad),
+            nn.BatchNorm3d(init_nodes*2),
+            nn.LeakyReLU(),
+            nn.MaxPool3d(kernel_size=2, stride=2),
+            nn.Dropout(drop)
+        )
+
+        out_dim = inp_dim/16
+        self.lin_unit_1 = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(int(init_nodes*2*out_dim), init_nodes*4),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(init_nodes*4),
+            nn.Dropout(drop)
+        )
+
+        self.lin_unit_2 = nn.Linear(init_nodes*4,1)
+        self.final = nn.Sigmoid()
+
+    def forward(self,x):
+        out = self.conv_unit_1(x)
+        out = self.conv_unit_2(out)
+        out = self.lin_unit_1(out)
+        out = self.lin_unit_2(out)
+        out = self.final(out)
+        return out.squeeze(1)
---
+++ tumor_utils.py
@@ -0,0 +1,205 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset
+import torchvision.transforms as tr
+import torchvision.transforms.functional as fc
+from torchvision.io import read_image
+
+from sklearn.metrics import accuracy_score
+from matplotlib import pyplot as plt
+
+import pandas as pd
+
+from time import time
+
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+# test data just transfromed into the same size 
+test_transform = tr.Compose([
+    tr.Resize(size=(96,96), antialias=True),
+    tr.Normalize(mean=[0.299], std=[0.137])
+    ])
+# transforms for data augmentation
+augm_transform = (
+    tr.Compose([
+    tr.Resize(size=(99, 99), antialias=True),
+    tr.RandomVerticalFlip(p=0.8),
+    tr.RandomCrop(size=(96,96)),
+    tr.Normalize(mean=[0.299], std=[0.137])
+]),
+tr.Compose([
+    tr.Resize(size=(99, 99), antialias=True),
+    tr.RandomHorizontalFlip(p=0.8),
+    tr.RandomCrop(size=(96,96)),
+    tr.Normalize(mean=[0.299], std=[0.137])
+]),
+tr.Compose([
+    tr.Resize(size=(112,112), antialias=True),
+    tr.RandomRotation(degrees=(3,9),expand=True),
+    tr.CenterCrop(size=(96,96)),
+    tr.Normalize(mean=[0.299], std=[0.137]),
+    ]),
+tr.Compose([
+    tr.Resize(size=(112,112), antialias=True),
+    tr.RandomPerspective(distortion_scale=0.15, p=0.8),
+    tr.CenterCrop(size=(96,96)),
+    tr.Normalize(mean=[0.299], std=[0.137]),
+    ])
+    )
+
+
+def image_loader(img_dir ='./data/STAN_patches_lbls/', train_bs = 16, data_3D = False):
+
+    """ utility function for data curation,
+    reads the images and labels from folders and creates test and train datasets and data loaders
+    train data are augmented because of small size of current dataset
+    Args:   img_dir | String - the relative path to main folder with data library
+            train_bs  - train batch size; for the test dataset, all data will be loaded at once
+            data_3D | bool- if False, the data will be converted to the 2D 96x96 images of 5 channels(for loading into 2D CNNs),
+                            if True,  the data will be converted to the 3D 5x96x96 images of 1 channel (for 3D CNN)
+
+    Out: test_data_loader, train_data_loader (as Torch DataLoaders)
+    """
+    label_path = img_dir + 'labels/STAN_labels.csv'
+    labels_df = pd.read_csv(label_path, delimiter=',', header=0)
+    test_labels = []
+    train_labels = []
+    # 3D / 2D
+    if data_3D:
+        test_img_tensor = torch.empty((0,1,5,96,96), dtype=torch.float, device=device)
+    else:
+        test_img_tensor = torch.empty((0,5,96,96), dtype=torch.float, device=device)
+    # 3D /2D
+    if data_3D:
+        train_img_tensor = torch.empty((0,1,5,96,96), dtype=torch.float, device=device)
+    else:
+        train_img_tensor = torch.empty((0,5,96,96), dtype=torch.float, device=device)
+    # iterating through the folders and reading stack of monochrome images
+    for idx, row in labels_df.iterrows():
+        subfolder = labels_df.UID.iloc[idx]
+        y_label = float(row['MUT_STATUS'])
+        # the list of tensors representing a stack of 5 images(slices)
+        imgs = []
+        for sl in range(0,5):
+            img_path = img_dir + 'patches/' + subfolder + f'/{subfolder}__sl_{sl}.png'
+            img = fc.convert_image_dtype(read_image(img_path))
+            imgs.append(img)
+        # each list image is transformed into  [5, 96, 96] tensor to represent a single X feature
+        load_img = torch.cat(imgs,dim=0)
+        test_img = test_transform(load_img).unsqueeze_(0)
+
+        # concatenating the features into a single test data tensor
+        # 3D /2D
+        if data_3D:
+            test_img_tensor = torch.cat((test_img_tensor, torch.unsqueeze(test_img, 0)), dim=0)
+        else:
+            test_img_tensor = torch.cat((test_img_tensor, test_img), dim=0)
+        # corresponding labels
+        test_labels.append(y_label)
+
+        # for training data augmentation, each loaded 5-stack image is cloned and transformed into 4 different tensors
+        for aug_tr in augm_transform:
+            train_img = aug_tr(load_img).unsqueeze_(0)
+            # concatenating the features into a single training data tensor
+            # 3D / 2D
+            if data_3D:
+                train_img_tensor = torch.cat((train_img_tensor, torch.unsqueeze(train_img,0)), dim=0)
+            else:
+                train_img_tensor = torch.cat((train_img_tensor, train_img), dim=0)
+            # corresponding labels
+            train_labels.append(y_label)
+    test_label_tensor = torch.Tensor(test_labels, device=device)
+    train_label_tensor = torch.Tensor(train_labels, device=device)
+
+    # data loaders to be used in the CNNS
+    test_dataset = TensorDataset(test_img_tensor, test_label_tensor)
+    train_dataset = TensorDataset(train_img_tensor, train_label_tensor)
+    train_data_loader = torch.utils.data.DataLoader(dataset= train_dataset, batch_size=train_bs, shuffle=True)
+    test_data_loader = torch.utils.data.DataLoader(dataset= test_dataset, batch_size=len(test_labels), shuffle=True)
+    
+    return train_data_loader, test_data_loader
+
+""" model evaluation utilities """
+
+# accuracy evaluation
+def accuracy_rate(predicted, true_y):
+    pred_y = [1 if pred > 0.5 else 0 for pred in predicted]
+    acc_score = accuracy_score(true_y, pred_y)
+    return acc_score
+
+# false negatives evaluation - important for the specific task (tumor classification)
+def false_neg_rate(predicted, true_y):
+    pred_y = [1 if pred > 0.5 else 0 for pred in predicted]
+    false_neg = [pr for pr, tr in zip(pred_y, true_y) if int(tr)==1 and pr==0 ]
+    return len(false_neg)/len(pred_y)
+
+
+def cnn_image_classifier(model, train_loader, test_loader, model_run_tag = '',
+                     epochs=10, learn_rate= 0.01, criterion = nn.BCELoss()):
+    
+    """ The 'wrapper' function which performs model training and evaluation, 
+            reports performance metrics, plots the learning curves and saves the model state
+    Args:   
+            CNN model, 
+            train_loader - multiple batches of training data, 
+            test_loader - single batch of data,
+            model_run_tag - to be used in the file names for learning plots and model state
+            number of epochs to train, learning rate, criterion 
+    Returns: final accuracy, false negatives rate and total execution time
+    """
+
+    test_data = iter(test_loader)
+    test_images, test_labels = next(test_data)
+    # optimizer  = torch.optim.Adam(model.parameters(), lr=learn_rate)
+    optimizer = torch.optim.Adagrad(model.parameters(), lr=learn_rate)
+    start_tm = time()
+    train_loss = []
+    train_accuracy = []
+    validation_accuracy = []
+    for ep in range(epochs):  
+        running_loss = 0
+        ave_accuracy = 0  #epoch accuracy, averaged over batches
+        with torch.no_grad():
+            test_output = model(test_images.to(device))
+        validation_accuracy.append(accuracy_rate(test_output, test_labels))
+        for count, (images, labels) in enumerate(train_loader):
+            images = images.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
+            tr_outputs = model(images)
+            ave_accuracy += accuracy_rate(tr_outputs, labels)
+            loss = criterion(tr_outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss +=  loss.item()
+        train_loss.append(running_loss/(count+1))
+        train_accuracy.append(ave_accuracy/(count+1))
+        print(f'loss: {running_loss} accuracy: {validation_accuracy[-1]}')
+    end_tm = time()
+
+    # reporting results
+    fig, axs = plt.subplots(1,2, figsize=(12,6))
+    axs[0].plot(range(1,epochs+1),train_loss)
+    axs[0].set_title('loss over training cycle')
+
+    axs[1].plot(range(1,epochs+1), train_accuracy, label="train accuracy")
+    axs[1].plot(range(1,epochs+1), validation_accuracy, '--', label="test accuracy")
+    axs[1].set_title('accuracy tracking')
+    axs[1].legend()
+    fig.suptitle(f'training of {model.name} model {model_run_tag}')
+    plt.savefig(f'./reports/{model.name}_{model_run_tag}_ep{epochs}_lr_{int(1000*learn_rate)}.svg', format='svg')
+    plt.show()
+    torch.save(model.state_dict(), f'./models/{model.name}_{model_run_tag}.pkl')
+    # evaluating model accuracy
+    with torch.no_grad():
+        test_output = model(test_images)
+    
+    exec_time = end_tm - start_tm
+    final_accur = accuracy_rate(test_output, test_labels)
+    false_negs = false_neg_rate(test_output, test_labels)
+
+    return final_accur, false_negs, exec_time
